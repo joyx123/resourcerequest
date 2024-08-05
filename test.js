53ae08deea699eec4a325d49660f280211541d17
joy <joyxia123@outlook.com>
2024-08-05 11:41:23 -0500

    update

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
+require('dotenv').config()
+
+const api_key = process.env.MISHAS_API_KEY;
+
+console.log(api_key);
+
 function clickForm(){
     
 }
 
 function startForm() {
     window.location.href = 'form.html';
+}
+
+function apikey(){
+    return process.env[MISHAS_API_KEY]
 }
